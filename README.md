d29eea038d1b09e631e5cf918a6f2b186b714787
ssttkkl <huang.wen.long@hotmail.com>
2020-05-11 09:23:17 +0800

    实现功能的开启和关闭 (#1)

--- README.md
+++ README.md
@@ -9,10 +9,11 @@ MiraiPixiv
 
 依赖库：[kuriyama](https://github.com/NatriumLab/python-mirai) [pixivpy](https://github.com/upbit/pixivpy) [json5](https://github.com/json5/json5) [pillow](https://github.com/python-pillow/Pillow)
 
-初次使用请将settings.template.json更名为settings.json，并填写mirai与pixiv栏目的信息。
+初次使用请将settings.template.json拷贝为settings.json，并填写mirai与pixiv栏目的信息。
 
 - 看看榜*1-20*：查看pixiv榜单的第1到第20名（可省略*1-20*）
 - 来张*FGO*图：搜索关键字*FGO*，从搜索结果随机抽选插画
+- 来张*森倉円*老师的作品：搜索画师*森倉円*，从该画师的插画列表里随机抽选插画
 - 看看图*114514*：查看id为*114514*的插画
 - 来张私家车：从书签中随机抽选插画
 
--- bot.py
+++ bot.py
@@ -9,28 +9,51 @@ from settings import settings
 
 qq = settings["mirai"]["qq"]
 authKey = settings["mirai"]["auth_key"]
-mirai_api_http_locate = f"""localhost:{settings["mirai"]["port"]}/"""
+port = settings["mirai"]["port"]
+mirai_api_http_locate = f"localhost:{port}/"
 if settings["mirai"]["enable_websocket"]:
     mirai_api_http_locate = mirai_api_http_locate + "ws"
 
-bot = Mirai(f"mirai://{mirai_api_http_locate}?authKey={authKey}&qq={qq}")
+url = f"mirai://{mirai_api_http_locate}?authKey={authKey}&qq={qq}"
+print("Connecting " + url)
+bot = Mirai(url)
+
+group_function = settings["function"]["group"]
+friend_function = settings["function"]["friend"]
 
 
 @bot.receiver("GroupMessage")
 async def group_receiver(bot: Mirai, source: Source, group: Group, message: MessageChain):
-    await pixiv_ranking_provider.receive(bot, source, group, message)
-    await pixiv_illust_provider.receive(bot, source, group, message)
-    await pixiv_shuffle_illust_provider.receive(bot, source, group, message)
-    await pixiv_shuffle_illustrator_illust_provider.receive(bot, source, group, message)
+    if group_function["listen"] is not None and group.id not in group_function["listen"]:
+        return
+
+    if group_function["ranking"]:
+        await pixiv_ranking_provider.receive(bot, source, group, message)
+    if group_function["illust"]:
+        await pixiv_illust_provider.receive(bot, source, group, message)
+    if group_function["shuffle_illust"]:
+        await pixiv_shuffle_illust_provider.receive(bot, source, group, message)
+    if group_function["shuffle_bookmarks"]:
+        await pixiv_shuffle_bookmarks_provider.receive(bot, source, group, message)
+    if group_function["shuffle_illustrator"]:
+        await pixiv_shuffle_illustrator_illust_provider.receive(bot, source, group, message)
 
 
 @bot.receiver("FriendMessage")
 async def friend_receiver(bot: Mirai, source: Source, friend: Friend, message: MessageChain):
-    await pixiv_ranking_provider.receive(bot, source, friend, message)
-    await pixiv_illust_provider.receive(bot, source, friend, message)
-    await pixiv_shuffle_illust_provider.receive(bot, source, friend, message)
-    await pixiv_shuffle_bookmarks_provider.receive(bot, source, friend, message)
-    await pixiv_shuffle_illustrator_illust_provider.receive(bot, source, friend, message)
+    if friend_function["listen"] is not None and friend.id not in friend_function["listen"]:
+        return
+
+    if friend_function["ranking"]:
+        await pixiv_ranking_provider.receive(bot, source, friend, message)
+    if friend_function["illust"]:
+        await pixiv_illust_provider.receive(bot, source, friend, message)
+    if friend_function["shuffle_illust"]:
+        await pixiv_shuffle_illust_provider.receive(bot, source, friend, message)
+    if friend_function["shuffle_bookmarks"]:
+        await pixiv_shuffle_bookmarks_provider.receive(bot, source, friend, message)
+    if friend_function["shuffle_illustrator"]:
+        await pixiv_shuffle_illustrator_illust_provider.receive(bot, source, friend, message)
 
 
 if __name__ == "__main__":
