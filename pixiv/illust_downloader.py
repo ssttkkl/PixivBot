080790c21d23858deb0152638f72f0e1b71fb303
ssttkkl <huang.wen.long@hotmail.com>
2020-07-23 21:08:00 +0800

    更改图片下载/压缩策略，加入图片过期设置

--- pixiv/illust_downloader.py
+++ pixiv/illust_downloader.py
@@ -1,8 +1,12 @@
 import os
+import shutil
+import time
 import typing as T
+from io import BytesIO
 from pathlib import Path
 
-from PIL import Image
+import aiofiles
+from PIL import Image, ImageFile
 
 from utils import settings, launch, log
 from .pixiv_api import papi
@@ -10,9 +14,9 @@ from .pixiv_api import papi
 compress: bool = settings["illust"]["compress"]
 compress_size: int = settings["illust"]["compress_size"]
 compress_quantity: int = settings["illust"]["compress_quantity"]
+download_outdated_time: int = settings["illust"]["download_outdated_time"]
 download_quantity: str = settings["illust"]["download_quantity"]
 download_dir: str = settings["illust"]["download_dir"]
-download_replace: bool = settings["illust"]["download_replace"]
 domain: T.Optional[str] = settings["illust"]["domain"]
 
 
@@ -39,42 +43,48 @@ async def cache_illust(illust: dict) -> Path:
     filename = os.path.basename(url)
     filepath = dirpath.joinpath(filename)
 
-    # 上面的文件名将后缀改为jpg的文件名（用于检查是否存在压缩过的文件）
-    filename_compressed = os.path.splitext(filename)[0] + ".compressed.jpg"
-    filepath_compressed = dirpath.joinpath(filename_compressed)
-
-    if not download_replace:
-        if filepath_compressed.exists():
-            return filepath_compressed
-        elif filepath.exists():
+    if filepath.exists():
+        now = time.time()
+        mtime = filepath.stat().st_mtime
+        if download_outdated_time is None or now - mtime <= download_outdated_time:
             return filepath
 
-    await launch(papi.download, url=url, path=dirpath, name=filename, replace=True)
+    data = await launch(__fetch_data, url=url)
+
+    if compress:
+        data = await launch(__compress_illust, data)
+
+    async with aiofiles.open(filepath, "wb") as f:
+        await f.write(data)
     log.debug(f"downloaded to {filepath}")
+    return filepath
 
-    if not compress:
-        return filepath
+
+def __fetch_data(url: str, referer='https://app-api.pixiv.net/') -> bytes:
+    with BytesIO() as bio:
+        rsp = papi.requests_call('GET', url, headers={'Referer': referer}, stream=True)
+        shutil.copyfileobj(rsp.raw, bio)
+        del rsp
+        return bio.getvalue()
+
+
+def __compress_illust(data: bytes) -> bytes:
+    """
+    压缩图片（图片以bytes形式传递）
+    :param data: 图片
+    """
+    p = ImageFile.Parser()
+    p.feed(data)
+    img = p.close()
+
+    w, h = img.size
+    if w > compress_size or h > compress_size:
+        ratio = min(compress_size / w, compress_size / h)
+        img_cp = img.resize((int(ratio * w), int(ratio * h)), Image.ANTIALIAS)
     else:
-        await launch(compress_illust, filepath, filepath_compressed)
-        # os.remove(str(filepath))
-        log.debug(f"compressed to {filepath_compressed}")
-        return filepath_compressed
+        img_cp = img.copy()
+    img_cp = img_cp.convert("RGB")
 
-
-def compress_illust(filepath: Path, filepath_compressed: Path):
-    """
-    压缩一个已经保存的illust
-    :param filepath: 原illust保存的路径
-    :param filepath_compressed: 压缩后的illust保存的路径
-    """
-
-    with Image.open(filepath) as img:
-        w, h = img.size
-        if w > compress_size or h > compress_size:
-            ratio = min(compress_size / w, compress_size / h)
-            img_cp = img.resize((int(ratio * w), int(ratio * h)), Image.ANTIALIAS)
-        else:
-            img_cp = img.copy()
-        img_cp = img_cp.convert("RGB")
-
-    img_cp.save(filepath_compressed, format="JPEG", optimize=True, quantity=compress_quantity)
+    with BytesIO() as bio:
+        img_cp.save(bio, format="JPEG", optimize=True, quantity=compress_quantity)
+        return bio.getvalue()
