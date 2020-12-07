5ab4a923fb7be68c2e0c3e9fea0c36e995704a68
ssttkkl <huang.wen.long@hotmail.com>
2020-12-07 14:07:33 +0800

    使用sortedcontainers管理handler优先度

--- handler/handler_manager.py
+++ handler/handler_manager.py
@@ -7,19 +7,14 @@ from loguru import logger
 
 from handler.abstract_message_handler import AbstractMessageHandler
 from utils import reply
+from sortedcontainers import SortedKeyList
 
 
 class HandlerManager:
-    handlers: T.Dict[str, AbstractMessageHandler]
-    handler_priority: T.Dict[str, int]
-    handler_allow_friend: T.Dict[str, T.Optional[T.Sequence[int]]]
-    handler_allow_group: T.Dict[str, T.Optional[T.Sequence[int]]]
+    handlers: SortedKeyList
 
     def __init__(self, bcc: Broadcast):
-        self.handlers = {}
-        self.handler_priority = {}
-        self.handler_allow_friend = {}
-        self.handler_allow_group = {}
+        self.handlers = SortedKeyList(key=lambda x: -x["priority"]) # 为了按优先级降序排序
 
         @bcc.receiver(FriendMessage)
         async def on_receive_friend_message(app: GraiaMiraiApplication,
@@ -34,16 +29,18 @@ class HandlerManager:
             await self.__on_receive(app, group, message)
 
     def register(self, handler: AbstractMessageHandler, **kwargs):
-        self.handlers[handler.tag] = handler
-        self.handler_priority[handler.tag] = kwargs.get("priority", 8)
-        self.handler_allow_friend[handler.tag] = kwargs.get("allow_friend", None)
-        self.handler_allow_group[handler.tag] = kwargs.get("allow_group", None)
+        self.handlers.add({
+            "handler": handler,
+            "priority": kwargs.get("priority", 8),
+            "allow_friend": kwargs.get("allow_friend", None),
+            "allow_group": kwargs.get("allow_group", None)
+        })
 
     def unregister(self, handler: AbstractMessageHandler):
-        del (self.handlers[handler.tag])
-        del (self.handler_priority[handler.tag])
-        del (self.handler_allow_friend[handler.tag])
-        del (self.handler_allow_group[handler.tag])
+        for h in self.handlers:
+            if h["handler"] == handler:
+                self.handlers.remove(h)
+                break
 
     async def __on_receive(self, app: GraiaMiraiApplication,
                            subject: T.Union[Group, Friend],
@@ -74,22 +71,17 @@ class HandlerManager:
         consumer_task = asyncio.create_task(consumer(channel))
 
         try:
-            handlers = []
-            for tag in self.handlers:
-                handlers.append(self.handlers[tag])
-            handlers.sort(key=lambda x: self.handler_priority[x.tag])
-
-            for handler in handlers:
-                handler: AbstractMessageHandler
+            for h in self.handlers:
+                handler: AbstractMessageHandler = h["handler"]
+                allow_group: T.Optional[T.Sequence[int]] = h["allow_group"]
+                allow_friend: T.Optional[T.Sequence[int]] = h["allow_friend"]
 
                 # 检查发送者是否有权限
                 if isinstance(subject, Group):
-                    if (self.handler_allow_group[handler.tag] is not None) \
-                            and (subject.id not in self.handler_allow_group[handler.tag]):
+                    if (allow_group is not None) and (subject.id not in allow_group):
                         continue
                 elif isinstance(subject, Friend):
-                    if (self.handler_allow_friend[handler.tag] is not None) \
-                            and (subject.id not in self.handler_allow_friend[handler.tag]):
+                    if (allow_friend is not None) and (subject.id not in allow_friend):
                         continue
 
                 # 若handler拦截了这条消息
